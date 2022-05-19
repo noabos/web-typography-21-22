53426794bb6db3972c4fa8aa3c0447794eda52b9
noabos <74159609+noabos@users.noreply.github.com>
2022-05-19 15:23:35 +0200

    Add files via upload

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,28 @@
-// Hier kan je tijden toevoegen in secondes
-// Op die mommenten komt er een class op de body.
+
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+    0, //sound 0 ✅ //
+    9, //sound 1 ✅ //
+    12, //sound 2 ✅ //
+    15.5, //sound 3 ✅ //
+    16, //sound 4 ✅ //
+    25, //sound 5  ✅ //
+    25.5, //sound 6   ✅ //
+    31, //sound 7 ✅ //
+    31.5, //sound 8 ✅ //
+    33, //sound 9 ✅ //
+    61, //sound 10 ✅ //
+    75, //sound 11  ✅ //
+    //Deel2
+    86.2, //sound12 ✅ //
+    87.7, //sound13 ✅ //
+    103.1, //sound14 ✅ //
+    105.7, //sound15 ✅ //
+    109.4, //sound16 ✅ //
+    115.9, //sound17 ✅ //
+    122.5, //sound18 ✅ //
+    124.3, //sound19 ✅ //
+    126.6, //sound20 ✅ //
 ];
+
+
+
